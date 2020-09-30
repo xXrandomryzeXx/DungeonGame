ee74a88e3d903fdd688ffacfb06510c5f8125e96
Alexander Belchev <randomryze1@gmail.com>
2020-09-30 14:52:05 +0300

    MapCreation. You can call in a function to add an array to the map with x y position with 0,0 being top left of the array

--- src/com/studio194/Main.java
+++ src/com/studio194/Main.java
@@ -4,15 +4,25 @@ import java.lang.String;
 public class Main {
     private static String[][] _map = new String[10][20];
 
+    String [][] array = new String[10][10];
+
+    static String[][] room = {
+            {"#  ", "#  ", "#  ", "#  "},
+            {"#  ", "   ", "   ", "#  "},
+            {"#  ", "   ", "   ", "#  "},
+            {"#  ", "#  ", "#  ", "#  "},
+    };
+
     public static void main(String[] args) {
         _generateMap();
+        addRoomToMap(room, 0, 0);
         _render();
     }
 
 
     private static void _generateMap(){
         for(int y = 0; y < _map.length; y++){
-            for(int x = 0; x < _map[0].length; x++){
+            for(int x = 0; x < _map[y].length; x++){
                 if(y == 0 || y == _map.length - 1 || x == 0 || x == _map[y].length - 1)
                     _map[y][x] = "#  ";
                 else{
@@ -22,9 +32,19 @@ public class Main {
         }
     }
 
+    private static void addRoomToMap(String[][] room, int posX, int posY){
+        for(int y = 0; y < _map.length; y++){
+            for(int x = 0; x < _map[y].length; x++){
+                if(x < room[0].length && y < room.length) {
+                    _map[y + posY][x + posX] = room[y][x];
+                }
+            }
+        }
+    }
+
     private static void _render(){
-        for (String[] strings : _map) {
-            for (String string : strings) {
+        for (String[] strings : _map) { //Gets a list of the y axis
+            for (String string : strings) { //Gets the list of the x axis on the y axis "strings"
                 System.out.print(string);
             }
             System.out.println();
